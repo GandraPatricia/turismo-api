d271e598a9c34fc7fd79681bafa571ed2bb93508
Claudio Matos <pathcau16@gmail.com>
2020-06-20 03:39:35 -0300

    Atualizando regras do CORS

--- app.js
+++ app.js
@@ -15,11 +15,8 @@ app.use(morgan('dev'))
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method == "OPTIONS") {
-        req.header("Access-Control-Allow-Methods", "PUT, POST, PATH, GET, DELETE");
-        return res.status(200).json({});
-    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATH, OPTIONS');
     next();
 });
 
