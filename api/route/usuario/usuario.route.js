38f41a962465967ec95d0a2624a5559e662d8cd1
Lucas Tiago <lucastiago45@gmail.com>
2020-06-20 01:36:11 -0300

    adicionando buscar usuario

--- api/route/usuario/usuario.route.js
+++ api/route/usuario/usuario.route.js
@@ -4,5 +4,6 @@ const usuarioController = require('../../controller/usuario/usuario.controller')
 
 router.post('/cadastrar', usuarioController.cadastrarUsuario)
 router.put('/editar', usuarioController.editarUsuario)
+router.get('/listar/:email', usuarioController.buscarUsuario)
 
 module.exports = router;
